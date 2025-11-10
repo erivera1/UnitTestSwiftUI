914629aa5c8f653b58fee6265db2e66892fb55d0
Eliric Rivera <eliricrivera@email.com>
2025-11-10 21:43:20 +0800

    Initial Commit

---
+++ UnitTestSwiftUI/UnitTestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestSwiftUIApp.swift
+//  UnitTestSwiftUI
+//
+//  Created by Eliric on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UnitTestSwiftUITests/UnitTestSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestSwiftUITests.swift
+//  UnitTestSwiftUITests
+//
+//  Created by Eliric on 11/10/25.
+//
+
+import Testing
+@testable import UnitTestSwiftUI
+
+struct UnitTestSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
